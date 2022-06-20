0b77dca57612c6287915ad33b9dfd85773c8b124
azizyelbay <34959497+azizyelbay@users.noreply.github.com>
2022-06-20 21:10:53 +0300

    First commit.

---
+++ webindexing-api/src/main/java/com/example/webindexing/WebindexingApplication.java
@@ -0,0 +1,13 @@
+package com.example.webindexing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebindexingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebindexingApplication.class, args);
+	}
+
+}
